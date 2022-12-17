868ef16af113a9a64e304a190c3fe44fed94bdf1
JJRubes <jamesfittongum@gmail.com>
2022-12-17 15:46:18 +1030

    parsing edges, and setup for recursive parsing

--- Makefile
+++ Makefile
@@ -1,2 +1,5 @@
 findBoxes.out: findBoxes.cpp
 	g++ findBoxes.cpp -Wall -o findBoxes.out
+
+breakDownBox.out: breakDownBox.cpp
+	g++ breakDownBox.cpp -Wall -o breakDownBox.out
--- breakDownBox.cpp
+++ breakDownBox.cpp
@@ -3,27 +3,12 @@
 #include<string>
 #include<vector>
 
-enum class Type {
-  INT,
-  CHAR,
-  STRING,
-  CUSTOM,
-  UNKOWN
-};
-
-// Will have a char to identify it
-// and store type information
-// including whether the type is known
-// for now, pins that have more that one type
-// can just be represented as different pins
-// located at the same location.
 class Pin {
   public:
-    Pin(char id, int x, int y, Type t = Type::UNKOWN) {
+    Pin(char id, int x, int y) {
       identifier = id;
       posX = x;
       posY = y;
-      type = t;
     }
     char getId() {
       return identifier;
@@ -34,57 +19,277 @@ class Pin {
     int y() {
       return posY;
     }
-    Type getType() {
-      return type;
-    }
-    std::string getTypeName() {
-      return typeName;
-    }
-    void setType(Type t, std::string tn = "") {
-      type = t;
-      typeName = tn;
+    void print() {
+      std::cout << "id: " << identifier << std::endl;
+      std::cout << "pos: (" << posX << ", " << posY << ")" << std::endl;
     }
   private:
     char identifier;
     int posX, posY;
-    Type type;
-    std::string typeName;
 };
 
-// path of a box seems to be
-// get bounds, and store contents ->
-// parse edges to find if def or desc ->
-// return as formed def or desc
+class CallBox {
+  public:
+    CallBox(std::string n, std::vector<Pin> p) {
+      name = n;
+      pins.swap(p);
+    }
+  private:
+    std::string name;
+    std::vector<Pin> pins;
+};
 
-// because of "figure it out" approach to types
-// there will probably be a need to reference
-// and modify functions
-// so maybe a parsed desc box has a reference to
-// the function.
-// if so then definitions will need to be parsed first
+class DefinitionBox {
+  public:
+    DefinitionBox(std::string n, std::vector<Pin> p, std::vector<DefinitionBox> defs, std::vector<CallBox> cs) {
+      name = n;
+      pins.swap(p);
+      definitions.swap(defs);
+      calls.swap(cs);
+    }
+  private:
+    std::string name;
+    std::vector<Pin> pins;
+    std::vector<DefinitionBox> definitions;
+    std::vector<CallBox> calls;
+};
 
-// The unparsed box is for storing gathered
-// information for access and manipulation.
-// It does not need to be representative of
-// how it will be run just what it is/contains.
-// It will store the it's position within
-// wherever it is defined.
-// It will store the string data, including
-// the edges. 
-// It will store a list of pins for itself.
-// They will need to be flexible because their
-// type might or might not be known at time of
-// creation.
-// It will need to store the boxes within it.
-// And how these boxes connect to others and itself.
 class UnparsedBox {
   public:
-    void print() {
+    UnparsedBox(int x, int y, std::vector<std::string> lines) {
+      tlx = x;
+      tly = y;
+      contents.swap(lines);
+
+      // this assumes every line is consistent in length
+      width = contents[0].size();
+      height = contents.size();
+
+      edgesParsed = false;
+      definition = false;
     }
+
+    bool isDef() {
+      if(!edgesParsed)
+        std::cout << "error" << std::endl;
+
+      return definition;
+    }
+
+    void parseEdges() {
+      if(edgesParsed)
+        std::cout << "error" << std::endl;
+
+      parseName();
+
+      // parse the pins
+      // top
+      for(int i = 0; i < width; i++) {
+        if(isPin(at(i, 0))) {
+          pins.push_back(Pin(at(i, 0), i, 0));
+        }
+      }
+
+      // left
+      for(int i = 0; i < height; i++) {
+        if(isPin(at(0, i))) {
+          pins.push_back(Pin(at(0, i), 0, i));
+        }
+      }
+
+      // right
+      for(int i = 0; i < height; i++) {
+        if(isPin(at(width - 1, i))) {
+          pins.push_back(Pin(at(width - 1, i), width - 1, i));
+        }
+      }
+
+      // bottom
+      for(int i = 0; i < width; i++) {
+        if(isPin(at(i, height - 1))) {
+          pins.push_back(Pin(at(i, height - 1), i, height - 1));
+        }
+      }
+
+      edgesParsed = true;
+    }
+
+    DefinitionBox makeDefinition() {
+      if(!edgesParsed)
+        std::cout << "error" << std::endl;
+
+      if(!definition)
+        std::cout << "error" << std::endl;
+
+      std::vector<DefinitionBox> definitions;
+      std::vector<CallBox> calls;
+
+      // find boxes would be here
+      // connect edges would be here
+
+      return DefinitionBox(name, pins, definitions, calls);
+    }
+
+    CallBox makeCall() {
+      if(!edgesParsed)
+        std::cout << "error" << std::endl;
+
+      if(definition)
+        std::cout << "error" << std::endl;
+
+      return CallBox(name, pins);
+    }
+
+    void print() {
+      if(!edgesParsed) {
+        for(std::string s : contents) {
+          std::cout << s << std::endl;
+        }
+      } else if(isDef()) {
+        std::cout << name << ": " << std::endl;
+        for(Pin p : pins) {
+          p.print();
+        }
+      }
+    }
+
+  private:
+    const bool isPin(char p) {
+      return p != '+' &&
+             p != '-' &&
+             p != '|' &&
+             p != ' ';
+    }
+
     char at(int x, int y) {
       return contents.at(y)[x];
     }
-  private:
+
+    void parseName() {
+      // check the top line
+      int start = scanHorizontal(0, true);
+      if(start != -1) {
+      std::cout << "here" << std::endl;
+        std::cout << "found start of name at " << start << std::endl;
+        int end = scanHorizontal(0, false);
+        std::cout << "found end of name at " << end << std::endl;
+        if(end < start) {
+          std::cout << "Error: found spaces on top edge that didn't contain a name." << std::endl;
+          return;
+        }
+        getName(start, end, 0, true);
+        return;
+      }
+
+      // left edge
+      start = scanVertical(0, true);
+      if(start != -1) {
+        int end = scanVertical(0, false);
+        if(end < start) {
+          std::cout << "Error: found spaces on left edge that didn't contain a name." << std::endl;
+          return;
+        }
+        getName(start, end, 0, false);
+        return;
+      }
+
+      // right edge
+      start = scanVertical(width - 1, true);
+      if(start != -1) {
+        int end = scanVertical(width - 1, false);
+        if(end < start) {
+          std::cout << "Error: found spaces on right edge that didn't contain a name." << std::endl;
+          return;
+        }
+        getName(start, end, width - 1, false);
+        return;
+      }
+
+      // bottom edge
+      start = scanHorizontal(height - 1, true);
+      if(start != -1) {
+        int end = scanHorizontal(height - 1, false);
+        if(end < start) {
+          std::cout << "Error: found spaces on bottom edge that didn't contain a name." << std::endl;
+          return;
+        }
+        getName(start, end, height - 1, true);
+        return;
+      }
+    }
+
+    int scanHorizontal(int line, bool forwards) {
+      std::cout << "scanning line " << line << std::endl;
+      bool spaces = false;
+      int last = -1;
+      int start = forwards ? 0 : width - 1;
+      int end = forwards ? width : -1;
+      int inc = forwards ? 1 : -1;
+      for(int i = start; inc * i < end; i += inc) {
+        std::cout << at(i, line) << ", " << i << std::endl;
+        if(spaces) {
+          if(at(i, line) != ' ') {
+            last = i;
+            break;
+          }
+        } else {
+          if(at(i, line) == ' ') {
+            spaces = true;
+          }
+        }
+      }
+      return last;
+    }
+
+    int scanVertical(int line, bool forwards) {
+      bool spaces = false;
+      int last = -1;
+      int start = forwards ? 0 : height - 1;
+      int end = forwards ? height : -1;
+      int inc = forwards ? 1 : -1;
+      for(int i = start; i < end; i += inc) {
+        if(spaces) {
+          if(at(line, i) != ' ') {
+            last = i;
+            break;
+          }
+        } else {
+          if(at(line, i) == ' ') {
+            spaces = true;
+          }
+        }
+      }
+      return last;
+    }
+
+    void getName(int start, int end, int line, bool horizontal) {
+      if(horizontal) {
+        name = std::string(contents.at(line), start, end - start + 1);
+        std::cout << "name is " << name << std::endl;
+        // then blank it
+        for(int i = start; i <= end; i++) {
+          contents[line][i] = '-';
+        }
+      } else {
+        name = std::string(start - end + 1, '.');
+        for(int i = start; i <= end; i++) {
+          name[i] = at(line, i);
+        }
+        // blank it
+        for(int i = start; i <= end; i++) {
+          contents[i][line] = '|';
+        }
+      }
+      definition = true;
+    }
+
+    bool edgesParsed;
+    // makes it easier to handle the
+    // def/call distinction
+    bool definition;
+
+    std::string name;
+
     // top left relative to parent box
     int tlx, tly;
 
@@ -96,116 +301,24 @@ class UnparsedBox {
 
     // the box's pins
     std::vector<Pin> pins;
-
-    // all the boxes contained within this box
-    std::vector<UnparsedBox> boxes;
-
-    // definitions
-    // functions
 };
-
-// has pins, and because a definition doesn't
-// connect to anything, it should be
-// unambiguous from the definition's
-// contents the types.
-// It makes sense to have a definition
-// for each overload.
-// The definition box will have a list of
-// definitions inside it
-// as well as a list of function boxes and
-// what they connect to.
-class DefinitionBox {
-  public:
-  private:
-    // none of these should have the UNKOWN type
-    std::vector<Pin> pins;
-    std::vector<DefinitionBox> definitions;
-    std::vector<DescriptionBox> descriptions;
-}
-
-class DescriptionBox {
-  public:
-    std::string name;
-    std::vector<Pin> pins;
-}
-class BreakDown {
-  public:
-    BreakDown(Box b);
-    void print();
-  private:
-    Box b;
-
-    // stores nested definitions within
-    // the current box
-    std::vector<Box> definitions;
-
-    // the name of the current definition
-    std::string name;
-
-    std::vector<unclaimedIO> io;
-
-    void parseEdges();
-};
-
-void BreakDown::parseEdges() {
-  // check the corners are '+'
-  if(b.at(b.topLeft()) != '+' || b.at(b.topRight()) != '+'
-      || b.at(b.bottomLeft()) != '+' || b.at(b.bottomRight()) != '+') {
-    // error
-    return;
-  }
-  
-}
-
-// have a box
-// which is just a list of strings
-// From the strings you can find from
-// the edges, 
-// the name
-// i/o pins
-// an i/o pin can be stored as a position, and a char
-// maybe also give a type if it is specified
-class UnclaimedIO {
-  public:
-    UnclaimedIo(char pin, int x, int y, std::string type = "unspecified") {
-      c = pin;
-      xpos = x;
-      ypos = y;
-      t = type;
-    }
-    char getPin() {
-      return c;
-    }
-    int getX() {
-      return x;
-    }
-    int getY() {
-      return y;
-    }
-  private:
-    char c;
-    // x and y relative to
-    // 0, 0 the top left corner
-    int xpos, ypos;
-    // this might be better stored as an enum but I'll deal with that later
-    std::string t;
-};
-
 
 int main() {
-  Box b(0, 23, 1);
-  b.add("+-------- main --------+");
-  b.add("|    string literal    |");
-  b.add("|  +----------------+  |");
-  b.add("|  | \"Hello World!\" |  |");
-  b.add("|  +-------o--------+  |");
-  b.add("|          |           |");
-  b.add("|      +---i---+       |");
-  b.add("|      | print |       |");
-  b.add("|      +-------+       |");
-  b.add("|                      |");
-  b.add("+----------------------+");
+  std::vector<std::string> lines;
+  lines.push_back("+-------- main --------+");
+  lines.push_back("|    string literal    |");
+  lines.push_back("|  +----------------+  |");
+  lines.push_back("|  | \"Hello World!\" |  |");
+  lines.push_back("|  +-------o--------+  |");
+  lines.push_back("|          |           |");
+  lines.push_back("|      +---i---+       |");
+  lines.push_back("|      | print |       |");
+  lines.push_back("|      +-------+       |");
+  lines.push_back("|                      |");
+  lines.push_back("+----------------------+");
 
-  BreakDown bd = BreakDown(b);
-  bd.print();
+  UnparsedBox b(1, 1, lines);
+  b.print();
+  b.parseEdges();
+  b.print();
 }
